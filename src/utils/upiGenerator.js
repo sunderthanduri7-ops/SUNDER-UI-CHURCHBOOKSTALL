fe5b90f76df742bed3abfbc32f0111870ba6e099
Your Name <your-email@example.com>
2026-06-25 13:14:53 +0530

    modified upi

--- src/utils/upiGenerator.js
+++ src/utils/upiGenerator.js
@@ -1,5 +1,5 @@
 export function generateUpiLink(amount){
-  const pa = 'subodhrethan@oksbi'
+  const pa = 'subodhrethan-1@oksbi'
   const pn = encodeURIComponent('ChurchBookStall')
   const am = Number(amount).toFixed(2)
   return `upi://pay?pa=${pa}&pn=${pn}&am=${am}&cu=INR`
